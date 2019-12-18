9b1d6446e664e99a79841a840a4dfbdcd98ec7e7
Kristen Doucette <cosmicnoir90@gmail.com>
2019-12-17 19:42:23 -0700

    Commit

--- DSA-LinkedList.js
+++ DSA-LinkedList.js
@@ -172,4 +172,103 @@ const main = () => {
   console.log(size(SLL));
 };
 
-main();
+// main();
+
+// Mystery Program:
+
+function WhatDoesThisProgramDo(lst) {
+  let current = lst.head;
+  while (current !== null) {
+    let newNode = current;
+    while (newNode.next !== null) {
+      if (newNode.next.value === current.value) {
+        newNode.next = newNode.next.next;
+      } else {
+        newNode = newNode.next;
+      }
+    }
+    current = current.next;
+  }
+}
+
+// Reverse a Linked List
+
+// Recursive
+const reverse = head => {
+  // If list is one node long
+  if (!head || !head.next) {
+    return head;
+  }
+
+  let temp = reverse(head.next);
+  head.next.next = head;
+  head.next = undefined;
+  return temp;
+};
+
+// 3rd from the end
+
+const findN = list => {
+  // Start at the head
+  let currNode = this.head;
+  // If the list is empty
+  if (!this.head) {
+    return null;
+  }
+  // Check for the item
+  while (currNode.value !== null) {
+    /* Return null if it's the end of the list 
+               and the item is not on the list */
+    if (currNode.next.next === null) {
+      return currNode;
+    } else {
+      // Otherwise, keep looking
+      currNode = currNode.next;
+    }
+  }
+};
+
+// Middle of a list
+
+const middle = list => {
+  let fastPoint = list.head;
+  let slowPoint = list.head;
+  while ((fastPoint.next !== null) & (fastPoint.next.next !== null)) {
+    fastPoint = fastPoint.next.next;
+    slowPoint = slowPoint.next;
+  }
+  return slowPoint;
+};
+
+// Cycle in a List
+
+const testList = new LinkedList();
+testList.insertFirst("Hey");
+testList.insertLast("Woa");
+testList.insertLast("Wowo");
+console.log(testList);
+
+const cycleList = list => {
+  let currNode = list.head;
+  // If the list is empty
+  if (!head) {
+    return null;
+  }
+
+  while (currNode.next !== null) {
+    if (currNode.next === null) {
+      return false;
+    } else {
+      // Otherwise, keep looking
+      currNode = currNode.next;
+    }
+  }
+};
+
+// Sorting a list
+
+const sort = list => {
+  if (list.head === null || list.head.next === null) {
+    return head;
+  }
+};
